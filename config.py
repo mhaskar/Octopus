e13ad647f776fcc06504c0fdeef8d5bffa1f5a87
mhaskar <m.askar@isecur1ty.org>
2021-03-05 12:00:46 +0000

    Initial commit for config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# unicorn path
+# if you don't have it check https://github.com/trustedsec/unicorn
+# unicorn used to convert powershell agent into vba macro
+
+unicorn_path = "/opt/redteaming/unicorn-3.15/unicorn.py"
